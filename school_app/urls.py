d5019bcca0dbcd78b979861c962dbedcb723376b
LuTyrn <lukas.tyrner@gmail.com>
2024-01-11 01:15:35 +0100

    First commit

---
+++ school_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('hello-world', views.hello_world, name="hello-world"),
+    path('welcome', views.welcome_to_school, name="welcome_to_school"),
+]
