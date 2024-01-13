806f887e922b015b1488f148a46063afa0ea2fa2
LuTyrn <lukas.tyrner@gmail.com>
2024-01-14 00:01:50 +0100

    L11 Endpointy Přidány základní endpointy

--- school_app/urls.py
+++ school_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('hello-world', views.hello_world, name="hello-world"),
     path('welcome', views.welcome_to_school, name="welcome_to_school"),
+    path('subjects', views.list_subjects, name="list_subjects")
 ]
