d5d527f99831b30a9b4faaa04abf4e62058c6170
LuTyrn <lukas.tyrner@gmail.com>
2024-01-15 03:00:57 +0100

    L12 CRUD endpointy

--- school_app/urls.py
+++ school_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('hello-world', views.hello_world, name="hello-world"),
     path('welcome', views.welcome_to_school, name="welcome_to_school"),
-    path('subjects', views.list_subjects, name="list_subjects")
+    path('subjects', views.list_subjects, name="list_subjects"),
+    path('subjects/<int:pk>', views.subject_detail, name="subject_detail")   # <int:pk> query parametr (integer:promena pk)
 ]
--- school_app/views.py
+++ school_app/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 import json
 
-subjects = [{"name": "Maths"}, {"name": "PE"}]
+subjects = [{"id": 1, "name": "Maths"}, {"id": 2, "name": "PE"}]
 
 def hello_world(request):
     return HttpResponse("Hello, world!")
@@ -27,3 +27,24 @@ def list_subjects(request):
         return JsonResponse(subject_dict, status=200)
     else:
         return HttpResponseNotFound("Sorry this method is not supported")
+    
+@csrf_exempt
+def subject_detail(request, pk):
+    global subjects
+
+    try:
+        subject = next(subject for subject in subjects if subject["id"] == pk)
+    except StopIteration:
+        return JsonResponse({"status": f"There is no subject with id {pk}"}, status=404)
+    
+    if request.method == "GET":
+        return JsonResponse(subject)
+    elif request.method == "PUT":
+        new_subject_bytes = request.body
+        new_subject = json.loads(new_subject_bytes)
+        new_subject_index = subjects.index(subject)
+        subjects[new_subject_index] = new_subject
+        return JsonResponse(new_subject, status=201)
+    elif request.method == "DELETE":
+        subjects = list(filter(lambda subject: subject["id"] != pk, subjects))
+        return HttpResponse(status=204)
